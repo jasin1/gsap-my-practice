0f30e5b9a41cb4c0806b3cf246236a25e57bbb7a
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-08-01 08:06:08 +0200

    animating door top

--- my-gsap.js
+++ my-gsap.js
@@ -2,3 +2,6 @@
 gsap.to(".fred", {x:400, scale:1.5, rotation:360, duration:3, repeat:1, yoyo:true});
 gsap.fromTo(".fred-2",{x:700, opacity:0},{x:400, rotation:360, duration:3, opacity:1, delay:2});
 
+//door-bottom
+//door-top
+gsap.to(".door-top",{scale:0, duration:3});
