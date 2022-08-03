420aef2afc4893b5ce2856e5ce94878e593dbf84
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-08-03 05:02:49 +0200

    timeline (not started)

--- my-gsap.js
+++ my-gsap.js
@@ -11,3 +11,5 @@ gsap.to(".door-bottom",{height:0, duration:myDuration, delay: myDelay});
 
 gsap.to(".door-white-top",{height:0, duration:0.4, delay: 0.5});
 gsap.to(".door-white-bottom",{height:0, duration:0.4, delay: 0.5});
+
+//gsap.timeline().from();
